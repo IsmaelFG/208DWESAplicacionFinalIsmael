0d134b097b32a638fe577642b29f1aa5b327d680
Ismael Ferreras <124872031+IsmaelFG@users.noreply.github.com>
2024-02-08 10:25:38 +0100

    Añadido acceso a PHPDoc

--- view/Layout.php
+++ view/Layout.php
@@ -29,6 +29,7 @@
                                 </a>
                             </div>
                             <div class="col text-end">
+                                <a href="doc/index.html" target="_blank" class="text-white mr-3" style="text-decoration: none;">PHPDoc</a>
                                 <a href="https://www.facebook.com/" target="_blank" class="text-white mr-3" style="text-decoration: none;">Imitada</a>
                                 <a href="webroot/documentos/curriculum.pdf" target="_blank" class="text-white mr-3" style="text-decoration: none;">Curriculum</a>
                                 <a href="https://github.com/IsmaelFG/208DWESAplicacionFinalIsmael" target="_blank">
